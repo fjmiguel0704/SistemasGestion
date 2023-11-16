ec94a2773d2b5cbad8617cc641d5b9305a1ca63f
fjmiguel <fernandoj.miguel@iesnervion.es>
2023-11-16 11:47:25 +0100

    terminado

---
+++ EjerciciosUd8/Ejercicio2/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Ejercicio2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ejercicio2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() { 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string nombre)
+        {
+            ViewBag.nombre = nombre;
+            return View("Saludo");
+        }
+    }
+}
