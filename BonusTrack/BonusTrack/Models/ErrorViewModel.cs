df0b912e528a22e0a8dddea4f156e86cf160ec91
fjmiguel <fernandoj.miguel@iesnervion.es>
2023-11-20 12:49:08 +0100

    hecho

---
+++ BonusTrack/BonusTrack/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BonusTrack.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
