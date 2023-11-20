849c50dab9bdcc058be30708a7c03c3ab2242dc2
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2023-11-20 23:22:17 +0100

    terminar

---
+++ BonusTrack/Biblioteca/clsMisiones.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class clsMisiones
+    {
+        #region atributos
+        private String nombreMision;
+        private String descripMision;
+        private String creditos;
+        #endregion
+
+        #region constructores
+        public clsMisiones()
+        {
+            nombreMision= "";
+        }
+
+        public clsMisiones(String mision)
+        {
+            this.nombreMision =mision;
+        }
+
+        public clsMisiones(String descripcionMision, String credito)
+        {
+            this.descripMision = descripcionMision;
+            this.creditos = credito;
+        }
+        #endregion
+
+        #region propiedades
+        public String NombreMision
+        {
+            get { return nombreMision; }
+            set { nombreMision = value; }
+        }
+        public String Descripcion
+        {
+            get { return descripMision; }
+            set { descripMision = value; }
+        }
+
+        public String Creditos
+        {
+            get { return creditos; }
+            set { creditos = value; }
+        }
+        #endregion
+    }
+}
---
+++ BonusTrack/BonusTrack/Models/ListadoMisiones.cs
@@ -0,0 +1,19 @@
+﻿using Biblioteca;
+namespace BonusTrack.Models
+{
+    static public class ListadoMisiones
+    {
+        static public List<clsMisiones> listarMisiones()
+        {
+            List<clsMisiones> listadoCompletoMisiones = new List<clsMisiones>()
+            {
+                new clsMisiones("Rescate de Baby Yoda"),
+                new clsMisiones("Recuperar armadura Beskar"),
+                new clsMisiones("Planeta Sorgon"),
+                new clsMisiones("Renacuajos")
+            };
+
+            return listadoCompletoMisiones;
+        }
+    }
+}
